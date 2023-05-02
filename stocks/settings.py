e3c20a88d4418bf4a593b4bec6a56af5afb9d214
Hammad-ul-Haq <hammadqureshi62@gmail.com>
2023-05-02 12:04:35 +0500

    setup postgres db and added railway deployment settings

--- stocks/settings.py
+++ stocks/settings.py
@@ -133,10 +133,21 @@ WSGI_APPLICATION = 'stocks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'FGK1mnrk2Zpyk2vmvvPT',
+        'HOST': 'containers-us-west-196.railway.app',
+        'PORT': '7924',
     }
 }
 
